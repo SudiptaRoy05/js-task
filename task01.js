cd00a0c99038ba65311cc0a23fb74765ddefb373
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-09-02 11:52:11 +0600

    task1

--- task01.js
+++ task01.js
@@ -1,1 +1,12 @@
 console.log("Task 1");
+
+function freeDrinks(burger){
+    if(burger >= 500){
+        console.log("Coke is free");
+    }else{
+        console.log("Coke: 30tk");
+    }
+}
+
+var burger = 800;
+freeDrinks(burger);

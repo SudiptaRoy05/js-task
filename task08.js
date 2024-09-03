5b5a5a0f0cc1933bcf5d3b87e6f56504b4785233
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-09-03 12:10:00 +0600

    task8

---
+++ task08.js
@@ -0,0 +1,8 @@
+var touristDestinations = ["Sundorbon", "Bandorban", "Sajek"];
+
+touristDestinations.push("TanguarHaur");
+
+touristDestinations.push("Jaflong", "BanglaBanda");
+touristDestinations.pop();
+console.log(touristDestinations);
+

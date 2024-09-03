347122f23ab410f0d56590dd0f50ed95ebb220bd
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-09-03 23:07:43 +0600

    revers an array

---
+++ task18.js
@@ -0,0 +1,10 @@
+const colors = ['red', 'blue', 'green', 'yellow', 'orange']
+
+const newColor  = [];
+for(let i=colors.length - 1 ; i >= 0 ; i--){
+    // console.log(colors[i]);
+    newColor.push(colors[i]);
+}
+
+console.log(colors);
+console.log(newColor);

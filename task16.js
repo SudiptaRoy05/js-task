7a123f3475c811851e637e6427cb4e0829039c79
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-09-03 14:09:27 +0600

    async

--- task16.js
+++ task16.js
@@ -1,6 +1,21 @@
-for (let i = 55; i<= 85; i++){
-    if(i%5 === 0){
-        continue;
-    }
-    console.log(i);
+// for (let i = 55; i<= 85; i++){
+//     if(i%5 === 0){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+async function fetchData() {
+  const url = "https://jsonplaceholder.typicode.com/todos/1";
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 }
+fetchData().then(data => {
+    if (data) {
+      console.log("Data received:", data);
+    } else {
+      console.log("Failed to fetch data.");
+    }
+  });
+

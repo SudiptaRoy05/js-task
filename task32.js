3db7fc8e17b7ef0b601050d054adc47d20274f50
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-09-05 23:57:36 +0600

    sum all2

--- task32.js
+++ task32.js
@@ -7,3 +7,16 @@ const sumAll = (arr) =>{
 }
 
 console.log(sumAll([3,5,8,9,6,100]));
+
+
+// Another way 
+
+const sumAll2 = (arr) =>{
+    var sum = 0;
+    for (const num of arr) {
+        sum += num
+    }
+    return sum;
+}
+
+console.log(sumAll2([3,5,9,6,100]));

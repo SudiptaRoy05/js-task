aa9f9b48e18a77abbacb6ec70c455f21eee67240
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-09-08 11:44:58 +0600

    b4t2 add another ways

--- b4T2.js
+++ b4T2.js
@@ -2,9 +2,9 @@ const checkName =(name)=>{
     if(typeof name !== "string"){
         return "Not valid"
     }
-    let word = name.slice(-1);
-    word = word.toLowerCase();
-    if(word === 'a' || word === 'y' || word === 'i' || word === 'e' || word === 'o' || word === 'u' || word === 'w'){
+    let lastWord = name.slice(-1);
+    lastWord = lastWord.toLowerCase();
+    if(lastWord === 'a' || lastWord === 'y' || lastWord === 'i' || lastWord === 'e' || lastWord === 'o' || lastWord === 'u' || lastWord === 'w'){
         return "good name";
     }
     return "Bad Name";
@@ -12,3 +12,43 @@ const checkName =(name)=>{
 
 
 console.log(checkName("Sudipta"));
+
+
+// another way 
+
+const checkName2 =(name)=>{
+    if(typeof name !== "string"){
+        return "Not valid"
+    }
+    let lastWord = name.slice(-1).toLowerCase();
+    const arr = ["a","y","i","e","o","u","w"];
+    for (const value of arr) {
+        if(value === lastWord){
+            return "Good Name";
+        }
+        else{
+            return "Bad name";
+        }
+    }
+   
+}
+
+console.log(checkName2("sudip"));
+
+
+
+// another way 
+
+const checkName3 =(name)=>{
+    if(typeof name !== "string"){
+        return "Not valid"
+    }
+    let lastWord = name.slice(-1).toLowerCase();
+    const arr = ["a","y","i","e","o","u","w"];
+    const result = arr.includes(lastWord);
+
+    return result ? "Good Name" : "Bad Name";
+   
+}
+
+console.log(checkName3("bilai"));
